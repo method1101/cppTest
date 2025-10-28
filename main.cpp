080850b1b274d855a002b385b372d05cd1d6ea7b
koorosh <method1101@gmail.com>
2025-10-28 23:50:26 +0330

    Printing a simple welcome message

--- main.cpp
+++ main.cpp
@@ -9,5 +9,14 @@ int main() {
   // Adding strings as the way they are not considered to be char *
   greeting = std::string ("* ") + ("Welcome, ") + fName + " *";
 
+  // Creating a simple border
+  const std::string border (greeting.size(), '*');
+
+  // Printing the final welcome message
+  std::cout << border << std::endl;
+  std::cout << greeting << std::endl;
+  std::cout << border << std::endl;
+
+
   return 0;
 }
