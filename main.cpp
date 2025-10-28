bd48aee90afa2e90df165e4696576eceeddee410
koorosh <method1101@gmail.com>
2025-10-28 22:32:21 +0330

    A clean slate...

--- main.cpp
+++ main.cpp
@@ -5,17 +5,7 @@ int main() {
   std::string fName = "";
   std::cin >> fName;
 
-  std::cout << "Enter your last name: ";
-  std::string lName = "";
-  std::cin >> lName;
 
-  std::cout << "Enter your age: ";
-  int age = 0;
-  std::cin >> age;
-
-  std::string greeting = "";
-  greeting = "Drear " + fName + " " + lName + ", " + "you are " + std::to_string(age) + " years old ;).";
-  std::cout << greeting << std::endl;
 
   return 0;
 }
